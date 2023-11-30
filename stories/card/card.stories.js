076d486fa2251901737ad4a9a620666fe417b014
Felipe Merchan <felipemerchanfuquen@gmail.com>
2023-11-29 23:56:02 -0500

    Creación del componente Card con JS y CSS

---
+++ stories/card/card.stories.js
@@ -0,0 +1,13 @@
+import _ from './card';
+
+export default {
+  title: 'Design System/Atoms/Button',
+}
+
+const Template = ({ ...args }) => _({ ...args })
+
+export const Default = Template.bind({});
+
+Default.args = {
+  
+}
