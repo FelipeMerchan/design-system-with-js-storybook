2f0c08b4f4592a1cdfb04fbbe2b709fbb1909848
Felipe Merchan <felipemerchanfuquen@gmail.com>
2023-12-03 01:24:11 -0500

    Parámetros y decoradores

--- stories/button/button.stories.js
+++ stories/button/button.stories.js
@@ -1,4 +1,5 @@
-import { createButton } from './button'
+import { createButton } from './button';
+/* import ButtonDocumentation from './button.md'; */
 
 export default {
   argTypes: {
@@ -23,15 +24,37 @@ export default {
       }
     },
   },
+  /* Permiten modificar la UI de Storybook: */
+  decorators: [(story) => {
+    const decorator = document.createElement('div');
+    decorator.style.margin = '24px';
+    decorator.appendChild(story());
+
+    return decorator;
+  }],
+  parameters: {
+    /* Parameters nos permite crear una documentación: */
+    docs: {
+      page: null,
+      description: {
+        /* Indica que va a ser un archivo de markdown el que vamos
+        a usar para la documentación. En el valor usamos nombre
+        del archivo que debe leer Storybook donde tenemos la documentación: */
+        /* component: ButtonDocumentation, */
+      },
+    }
+  },
+  tags: ['autodocs'],
   title: 'Design System/Atoms/Button',
-}
+};
 
 const Template = ({ label, ...args }) => {
   return createButton({ label, ...args });
-}
+};
 
-export const Default = Template.bind({})
+export const Default = Template.bind({});
+
 Default.args = {
   label: 'Button',
   style: 'filled',
-}
+};
